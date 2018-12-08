dc930e9c54c2f6342da4b69a88377f5552c1e462
samuelludwig <samuelludwig74@yahoo.com>
2018-12-08 11:46:50 -0500

    more component arranging done, all is currently enough to function

--- bugme/gui.py
+++ bugme/gui.py
@@ -48,10 +48,10 @@ token_change_frame = tkinter.Frame(window)
 changes_label = tkinter.Label(change_note_frame, text="Changes will take effect the next time BugMe is turned on")
 
 # [Button] ON
-on_button = tkinter.Button(on_off_frame, text="ON", width=4, command=turn_on)
+on_button = tkinter.Button(on_off_frame, text="ON", width=16, height=6,command=turn_on)
 
 # [Button] OFF
-off_button = tkinter.Button(on_off_frame, text="ON", width=4, command=turn_off)
+off_button = tkinter.Button(on_off_frame, text="OFF", width=16, height=6, command=turn_off)
 
 # [Label] Check and Alert Me Every...
 frequency_label_1 = tkinter.Label(frequency_frame, text="Check and alert me every")
@@ -63,7 +63,7 @@ frequency_input = tkinter.Entry(frequency_frame)
 frequency_label_2 = tkinter.Label(frequency_frame, text="minutes")
 
 # [Label] Change UTC Offset
-offset_label = tkinter.Label(utc_frame, text="Change UTC Offset")
+offset_label = tkinter.Label(utc_frame, text="Change UTC Offset ('+' or '-' followed by time in HH:MM format)")
 
 # [ListBox] POS/NEG Indicator
 offset_sign = tkinter.Listbox(utc_frame, selectmode="browse", width=3, height=2)
@@ -73,25 +73,45 @@ offset_sign.insert(0, "+", "-")
 offset_amount = tkinter.Entry(utc_frame, width=10)
 
 # [Label] Change Alert Sound
-change_alert_label = tkinter.Label(change_alert_frame)
+change_alert_label = tkinter.Label(change_alert_frame, text="Change Alert Sound, provide a URI or browse your system for files")
 
 # [TextEntry] Change Alert Audio (URI)
 alert_uri = tkinter.Entry(change_alert_frame)
 
 # [Button] Browse for Soundfile on System
-alert_browse = tkinter.Button(change_alert_frame, text="Browse")
+alert_browse = tkinter.Button(change_alert_frame, width=8, text="Browse")
 
 # [Label] Change User Token
 change_token_label = tkinter.Label(token_change_frame, text="Change user token:")
 
 # [TextEntry] User Token
-token_input = tkinter.Entry(token_change_frame)
+token_input = tkinter.Entry(token_change_frame, width=24)
 
-utc_frame.grid(row=0, column=0)
-offset_label.grid(row=0, column=0, sticky='w')
-offset_sign.grid(row=1, column=0, sticky='w')
-offset_amount.grid(row=1, column=1, sticky='w')
+utc_frame.grid(row=0, column=0, padx=16, pady=16, sticky='nsew')
+offset_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
+offset_sign.grid(row=1, column=0, sticky='nsew')
+offset_amount.grid(row=1, column=1, sticky='nsew')
 
+frequency_frame.grid(row=0, column=2, padx=16, pady=16, sticky='e')
+frequency_label_1.grid(row=0, column=0, sticky='nsew')
+frequency_input.grid(row=1, column=0, sticky='nsew')
+frequency_label_2.grid(row=1, column=1, sticky='nsew')
+
+change_alert_frame.grid(row=1, column=2, padx=16, pady=16, sticky='nsew')
+change_alert_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
+alert_uri.grid(row=1, column=0, sticky='nsew')
+alert_browse.grid(row=1, column=1, sticky='nsew')
+
+on_off_frame.grid(row=2, column=1, padx=16, pady=16, sticky='nsew')
+on_button.grid(row=0, column=0, sticky='nsew')
+off_button.grid(row=0, column=1, sticky='nsew')
+
+token_change_frame.grid(row=1, column=0, padx=16, pady=16, sticky='nsew')
+change_token_label.grid(row=0, column=0, sticky='nsew')
+token_input.grid(row=1, column=0, sticky='nsew')
+
+change_note_frame.grid(row=2, column=0, padx=16, pady=16, sticky='nsew')
+changes_label.grid(row=0, column=0, sticky='w')
 
 window.mainloop()
 
