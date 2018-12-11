ef7bb29b3a5e3f459083781f1505df9e670ba1b0
samuelludwig <samuelludwig74@yahoo.com>
2018-12-10 23:08:01 -0500

    added some documentation and an error check to see if sound file provided is invalid

--- bugme/overdueAlert.py
+++ bugme/overdueAlert.py
@@ -2,8 +2,16 @@ from pygame import mixer
 from time import sleep
 
 def play_alert(sound_file):
-    mixer.init()
-    mixer.music.load(sound_file)
-    mixer.music.play()
-    sleep(10)
-    mixer.music.stop()
+    """To be triggered by watch when overdue task encountered
+    
+    Plays the selected sound file if it is of proper format (.mp3), else should return
+    an error.
+    """
+    try:
+        mixer.init()
+        mixer.music.load(sound_file)
+        mixer.music.play()
+        sleep(10)
+        mixer.music.stop()
+    except:
+        print("Error: Improper File Format")
