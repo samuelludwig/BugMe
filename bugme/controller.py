f6201fc4177e4b746da12f638387be2b5cf3729b
samuelludwig <samuelludwig74@yahoo.com>
2019-01-27 18:41:24 -0500

    adds initial refactoring outline of the branch
    current issue to be worked on is in controller.py, if I uncomment the lines relating to turn_on and turn_off (lines 15-26) I am getting an AttributeError which I don't understand how to deal with yet.

--- bugme/controller.py
+++ bugme/controller.py
@@ -2,125 +2,37 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from time import sleep
-
 import json
 import multiprocessing
 import tkinter
-from bugme import getDueDates, overdueAlert
+from bugme import getDueDates, overdueAlert, view
 from datetime import datetime, timedelta
 
+### --- BEGIN UI COMPONENT DEFINITION --- ###
 
-#################################################################################################
-# TODO: Find a way to call watch repeatedly, which also stops when the off button is pressed    #
-#################################################################################################
-#                                                                                               # 
-#################################################################################################
+class Controller:
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.view = view.View(self.root)
+    #     self.root.on_button.bind('<Button-1>', self.turn_on)
+    #     self.root.off_button.bind('<Button-1>', self.turn_off)
 
-window = tkinter.Tk()
-# [Title] Window Title
-window.title("BugMe Control Panel")
+    # def turn_on(self, event):
+    #     print("On button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
+    #     # grab_user_data(token_input.get(), frequency_input.get(), offset_amount.get(), offset_sign.curselection(), alert_uri.get())
+    #     # watch_process.start()
 
-# [Frame] Changes Note Component Container - Holds:
-#   - [Label] Changes will take effect the next time BugMe is turned ON
-change_note_frame = tkinter.Frame(window)
+    # def turn_off(self, event):
+    #     print("Off button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
+    #     # watch_process.terminate()
+    #     # watch_process.join()
 
-# [Frame] ON/OFF Component Container - Holds:
-#   - [Button] ON
-#   - [Button] OFF
-on_off_frame = tkinter.Frame(window)
+    def run(self):
+        self.root.title("BugMe Control Panel")
+        self.root.deiconify()
+        self.root.mainloop()
 
-# [Frame] Alert Frequency Component Containter - Holds:
-#   - [Label] Check and Alert Me Every...
-#   - [TextEntry] Alert Frequency Setting (>= 1 minute)
-#   - [Label] ...minute(s)
-frequency_frame = tkinter.Frame(window)
-
-# [Frame] UTC Component Container - Holds:
-#   - [Label] Change UTC Offset
-#   - [ListBox] POS/NEG Indicator
-#   - [TextEntry] Time Offset in HH:MM Format
-utc_frame = tkinter.Frame(window)
-
-# [Frame] Change Alert Audio Component Container - Holds:
-#   - [Label] Change Alert Sound
-#   - [TextEntry] Change Alert Audio (URI)
-#   - [Button] Browse for Soundfile on System
-change_alert_frame = tkinter.Frame(window)
-
-# [Frame] Token Change Component Container - Holds:
-#   - [Label] Change User Token
-#   - [TextEntry] User Token
-token_change_frame = tkinter.Frame(window)
-
-
-# [Label] Changes will take effect the next time BugMe is turned ON
-changes_label = tkinter.Label(change_note_frame, text="Changes will take effect the next time BugMe is turned on")
-
-# [Button] ON
-on_button = tkinter.Button(on_off_frame, text="ON", width=16, height=6)
-
-# [Button] OFF
-off_button = tkinter.Button(on_off_frame, text="OFF", width=16, height=6)
-
-# [Label] Check and Alert Me Every...
-frequency_label_1 = tkinter.Label(frequency_frame, text="Check and alert me every")
-
-# [TextEntry] Alert Frequency Setting (>= 1 minute)
-frequency_input = tkinter.Entry(frequency_frame)
-
-# [Label] ...minute(s)
-frequency_label_2 = tkinter.Label(frequency_frame, text="minutes")
-
-# [Label] Change UTC Offset
-offset_label = tkinter.Label(utc_frame, text="Change UTC Offset ('+' or '-' followed by time in HH:MM format)")
-
-# [ListBox] POS/NEG Indicator
-offset_sign = tkinter.Listbox(utc_frame, selectmode="browse", width=3, height=2)
-offset_sign.insert(0, "+", "-")
-
-# [TextEntry] Time Offset in HH:MM Format
-offset_amount = tkinter.Entry(utc_frame, width=10)
-
-# [Label] Change Alert Sound
-change_alert_label = tkinter.Label(change_alert_frame, text="Change Alert Sound, provide a URI or browse your system for files")
-
-# [TextEntry] Change Alert Audio (URI)
-alert_uri = tkinter.Entry(change_alert_frame)
-
-# [Button] Browse for Soundfile on System
-alert_browse = tkinter.Button(change_alert_frame, width=8, text="Browse")
-
-# [Label] Change User Token
-change_token_label = tkinter.Label(token_change_frame, text="Change user token:")
-
-# [TextEntry] User Token
-token_input = tkinter.Entry(token_change_frame, width=48, show="*")
-
-utc_frame.grid(row=0, column=0, padx=16, pady=16, sticky='nsew')
-offset_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
-offset_sign.grid(row=1, column=0, sticky='nsew')
-offset_amount.grid(row=1, column=1, sticky='nsew')
-
-frequency_frame.grid(row=0, column=2, padx=16, pady=16, sticky='e')
-frequency_label_1.grid(row=0, column=0, sticky='nsew')
-frequency_input.grid(row=1, column=0, sticky='nsew')
-frequency_label_2.grid(row=1, column=1, sticky='nsew')
-
-change_alert_frame.grid(row=1, column=2, padx=16, pady=16, sticky='nsew')
-change_alert_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
-alert_uri.grid(row=1, column=0, sticky='nsew')
-alert_browse.grid(row=1, column=1, sticky='nsew')
-
-on_off_frame.grid(row=2, column=1, padx=16, pady=16, sticky='nsew')
-on_button.grid(row=0, column=0, sticky='nsew')
-off_button.grid(row=0, column=1, sticky='nsew')
-
-token_change_frame.grid(row=1, column=0, padx=16, pady=16, sticky='nsew')
-change_token_label.grid(row=0, column=0, sticky='nsew')
-token_input.grid(row=1, column=0, sticky='nsew')
-
-change_note_frame.grid(row=2, column=0, padx=16, pady=16, sticky='nsew')
-changes_label.grid(row=0, column=0, sticky='w')
+### --- END UI COMPONENTS --- ###
 
 def trigger(utc_offset, utc_sign, alert_uri):
         """Goes through list of converted dates and compares them to current time to see if they have passed.
@@ -159,8 +71,6 @@ def fill_fields():
         except:
             pass
 
-
-
 def grab_user_data(token, frequency, utc_offset, utc_sign, alert_uri):
     """Tosses entered user data into a JSON file (user_data.json)
     
@@ -190,7 +100,7 @@ def watch(token, frequency, utc_offset, utc_sign, alert_uri):
         Goes through the process of getting and converting the due dates of each task,
         then testing them to see if they are past due. If they are past due trigger() will call
         the play_alert() function. Runs as long as app is up, will update and check
-        tasks every x minutes.
+        tasks every (frequency) minutes.
         """
         last_time = (datetime.now() - timedelta(minutes=int(frequency)))
         
@@ -212,21 +122,6 @@ if __name__ == "__main__":
             data['user_info'][0]['utc_offset'], 
             data['user_info'][0]['utc_sign'], 
             data['user_info'][0]['alert_uri']))
-    
-    def turn_on(event):
-        print("On button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
-        grab_user_data(token_input.get(), frequency_input.get(), offset_amount.get(), offset_sign.curselection(), alert_uri.get())
-        watch_process.start()
-
-    def turn_off(event):
-        print("Off button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
-        watch_process.terminate()
-        watch_process.join()
-
-    on_button.bind('<Button-1>', turn_on)
-    off_button.bind('<Button-1>', turn_off)
-
-    window.mainloop()
 
 # call_watch_me()
 # while True:
---
+++ bugme/view.py
@@ -0,0 +1,68 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+import tkinter
+
+class View:
+    def __init__(self, root):
+        self.change_note_frame = tkinter.Frame(root)
+        self.on_off_frame = tkinter.Frame(root)
+        self.frequency_frame = tkinter.Frame(root)
+        self.utc_frame = tkinter.Frame(root)
+        self.change_alert_frame = tkinter.Frame(root)
+        self.token_change_frame = tkinter.Frame(root)
+        self.changes_label = tkinter.Label(self.change_note_frame, text="Changes will take effect the next time BugMe is turned on")
+        self.on_button = tkinter.Button(self.on_off_frame, text="ON", width=16, height=6)
+        self.off_button = tkinter.Button(self.on_off_frame, text="OFF", width=16, height=6)
+        self.frequency_label_1 = tkinter.Label(self.frequency_frame, text="Check and alert me every")
+        self.frequency_input = tkinter.Entry(self.frequency_frame)
+        self.frequency_label_2 = tkinter.Label(self.frequency_frame, text="minutes")
+        self.offset_label = tkinter.Label(self.utc_frame, text="Change UTC Offset ('+' or '-' followed by time in HH:MM format)")
+        self.offset_sign = tkinter.Listbox(self.utc_frame, selectmode="browse", width=3, height=2)
+        self.offset_sign.insert(0, "+", "-")
+        self.offset_amount = tkinter.Entry(self.utc_frame, width=10)
+        self.change_alert_label = tkinter.Label(self.change_alert_frame, text="Change Alert Sound, provide a URI or browse your system for files")
+        self.alert_uri = tkinter.Entry(self.change_alert_frame)
+        self.alert_browse = tkinter.Button(self.change_alert_frame, width=8, text="Browse")
+        self.change_token_label = tkinter.Label(self.token_change_frame, text="Change user token:")
+        self.token_input = tkinter.Entry(self.token_change_frame, width=48, show="*")
+        
+        self.utc_frame.grid(row=0, column=0, padx=16, pady=16, sticky='nsew')
+        self.offset_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
+        self.offset_sign.grid(row=1, column=0, sticky='nsew')
+        self.offset_amount.grid(row=1, column=1, sticky='nsew')
+
+        self.frequency_frame.grid(row=0, column=2, padx=16, pady=16, sticky='e')
+        self.frequency_label_1.grid(row=0, column=0, sticky='nsew')
+        self.frequency_input.grid(row=1, column=0, sticky='nsew')
+        self.frequency_label_2.grid(row=1, column=1, sticky='nsew')
+
+        self.change_alert_frame.grid(row=1, column=2, padx=16, pady=16, sticky='nsew')
+        self.change_alert_label.grid(row=0, column=0, columnspan=2, sticky='nsew')
+        self.alert_uri.grid(row=1, column=0, sticky='nsew')
+        self.alert_browse.grid(row=1, column=1, sticky='nsew')
+
+        self.on_off_frame.grid(row=2, column=1, padx=16, pady=16, sticky='nsew')
+        self.on_button.grid(row=0, column=0, sticky='nsew')
+        self.off_button.grid(row=0, column=1, sticky='nsew')
+
+        self.token_change_frame.grid(row=1, column=0, padx=16, pady=16, sticky='nsew')
+        self.change_token_label.grid(row=0, column=0, sticky='nsew')
+        self.token_input.grid(row=1, column=0, sticky='nsew')
+
+        self.change_note_frame.grid(row=2, column=0, padx=16, pady=16, sticky='nsew')
+        self.changes_label.grid(row=0, column=0, sticky='w')
+
+        root.on_button.bind('<Button-1>', self.turn_on)
+        root.off_button.bind('<Button-1>', self.turn_off)
+
+    def turn_on(self, event):
+        print("On button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
+        # grab_user_data(token_input.get(), frequency_input.get(), offset_amount.get(), offset_sign.curselection(), alert_uri.get())
+        # watch_process.start()
+
+    def turn_off(self, event):
+        print("Off button pressed!") # DEBUG PRINT: REMOVE THIS LATER #
+        # watch_process.terminate()
+        # watch_process.join()
